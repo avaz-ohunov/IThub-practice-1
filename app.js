d884105f206f3539856c419e091c8b64724ad8c2
avaz-ohunov <avaz14959@gmail.com>
2024-06-14 12:09:40 +0300

    Практика 1

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
 // app.js
 
-const express = require('express');
-const fs = require('fs');
-const https = require('https');
+const express = require("express");
+const fs = require("fs");
+const https = require("https");
 const app = express();
 
-const content = fs.readFileSync('data.json', 'utf8');
+const content = fs.readFileSync("data.json", "utf8");
 const keywordsToUrls = JSON.parse(content);
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/search/:keyword', (req, res) => {
+app.get("/search/:keyword", (req, res) => {
     const keyword = req.params.keyword;
     const urls = keywordsToUrls[keyword];
     if (urls) {
         res.json(urls);
     } else {
-        res.status(404).send('Ключевое слово не найдено');
+        res.status(404).send("Ключевое слово не найдено");
     }
 });
 
-app.get('/download/:url', (req, res) => {
+app.get("/download/:url", (req, res) => {
     const url = decodeURIComponent(req.params.url);
     
-    res.setHeader('Content-Type', 'text/event-stream');
-    res.setHeader('Cache-Control', 'no-cache');
-    res.setHeader('Connection', 'keep-alive');
+    res.setHeader("Content-Type", "text/event-stream");
+    res.setHeader("Cache-Control", "no-cache");
+    res.setHeader("Connection", "keep-alive");
 
     https.get(url, (response) => {
         if (response.statusCode !== 200) {
@@ -33,26 +33,26 @@ app.get('/download/:url', (req, res) => {
             return;
         }
 
-        const contentLength = parseInt(response.headers['content-length'], 10);
+        const contentLength = parseInt(response.headers["content-length"], 10);
         let bytesReceived = 0;
-        let content = '';
+        let content = "";
 
-        response.on('data', (chunk) => {
+        response.on("data", (chunk) => {
             bytesReceived += chunk.length;
-            content += chunk.toString('utf8');
+            content += chunk.toString("utf8");
             const progress = ((bytesReceived / contentLength) * 100).toFixed(2);
             res.write(`data: ${JSON.stringify({ progress })}\n\n`);
         });
 
-        response.on('end', () => {
+        response.on("end", () => {
             res.write(`data: ${JSON.stringify({ progress: 100, content })}\n\n`);
             res.end();
         });
 
-        response.on('error', (error) => {
+        response.on("error", (error) => {
             res.status(500).send(error.message);
         });
-    }).on('error', (error) => {
+    }).on("error", (error) => {
         res.status(500).send(error.message);
     });
 });
